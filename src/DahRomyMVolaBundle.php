c44b5a96c32c7dfbb3d833adff04c85331f7bb13
dahromy <romeorazaf@gmail.com>
2024-09-21 14:48:47 +0300

    refactor: remove unused methods and optimize deps
    Refactor code to eliminate unused methods in DahRomyMVolaBundle.php
    and optimize dependency imports in DahRomyMVolaExtension.php. This
    streamlines the code and reduces unnecessary dependencies.

--- src/DahRomyMVolaBundle.php
+++ src/DahRomyMVolaBundle.php
@@ -3,7 +3,6 @@
 namespace DahRomy\MVola;
 
 use DahRomy\MVola\DependencyInjection\DahRomyMVolaExtension;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -12,21 +11,5 @@ class DahRomyMVolaBundle extends Bundle
     public function getContainerExtension(): ?ExtensionInterface
     {
         return new DahRomyMVolaExtension();
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
-
-    /**
-     * @return ContainerInterface|null
-     */
-    public function getContainer(): ?ContainerInterface
-    {
-        return $this->container;
     }
 }
--- src/DependencyInjection/DahRomyMVolaExtension.php
+++ src/DependencyInjection/DahRomyMVolaExtension.php
@@ -4,8 +4,8 @@ namespace DahRomy\MVola\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class DahRomyMVolaExtension extends Extension
 {
